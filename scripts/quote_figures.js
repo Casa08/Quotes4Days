571d302ceb41777836c195fe82ef1637ba9163d1
Casa08 <163281668+Casa08@users.noreply.github.com>
2025-03-31 02:34:00 +0200

    Update quote_figures.js

--- scripts/quote_figures.js
+++ scripts/quote_figures.js
@@ -7,36 +7,36 @@ const contents = [
     {
       image: '../images/card_3.jpg',
       name: 'Albert Einstein',
-      link: 'albert-einstein.html'
+      link: '../html/albert-einstein.html'
     },
     {
       image: '../images/card_4.jpg',
       name: 'Jean-Paul Sartre',
-      link: 'jean-paul.html'
+      link: '../html/jean-paul.html'
     },
     {
       image: '../images/card_5.jpg',
       name: 'Nikola Tesla',
-      link: 'nikola-tesla.html'
+      link: '../html/nikola-tesla.html'
     },
     {
       image: '../images/card_6.jpg',
       name: 'Juansen Dizon',
-      link: 'juansen-dizon.html'
+      link: '../html/juansen-dizon.html'
     },
     {
       image: '../images/card_7.jpg',
       name: 'J.R.R Tolkien',
-      link: 'jrr-tolkien.html'
+      link: '../html/jrr-tolkien.html'
     },
     {
       image: '../images/card_8.jpg',
       name: 'Benjamin Franklin',
-      link: 'benjamin-franklin.html'
+      link: '../html/benjamin-franklin.html'
     },
     {
       image: '../images/card_9.jpg',
       name: 'Alfred Lord Tennyson',
-      link: 'alfred-tennyson.html'
+      link: '../html/alfred-tennyson.html'
     }
   ];
